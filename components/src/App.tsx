ccc8d01eb8d7954e446e3d13aaf65c72f58eba05
Omar Mohsin <ajna%K[Anl'=OcD>
2023-09-08 14:07:40 +0300

    fixing the ios part

--- components/src/App.tsx
+++ components/src/App.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Button } from 'react-native';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from '../../store/store';
@@ -6,33 +6,32 @@ import { Home } from './Home';
 import { Cart } from './Cart';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-
+import { enableScreens } from 'react-native-screens';
 
 export default function App(): JSX.Element {
+  enableScreens(); // Enable screens before creating the navigator
+
   const Tab = createBottomTabNavigator();
 
   return (
+    
     <Provider store={store}>
 
-      <NavigationContainer>
+
+ <NavigationContainer>
         <Tab.Navigator>
           <Tab.Screen
             name="Home"
             component={Home}
-            options={{
-              title: 'Store',
-            }}
+            options={{ title : 'Home' }}
           />
           <Tab.Screen
             name="Cart"
             component={Cart}
-            options={{
-              title: 'Cart',
-            }}
+            options={{ title : 'cart' }}
           />
         </Tab.Navigator>
-      </NavigationContainer>
+      </NavigationContainer> 
     </Provider>
   );
 }
