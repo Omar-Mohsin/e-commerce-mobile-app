6d0cf4d6a0e7ca0961eb396da6b0a4ff1e2b3fbf
Omar Mohsin <ajna%K[Anl'=OcD>
2023-09-26 14:09:47 +0300

    adding a image slider

--- components/src/HomePage/Detail.tsx
+++ components/src/HomePage/Detail.tsx
@@ -11,10 +11,21 @@ import {
 const Detail = ({route}: any): JSX.Element => {
   const {product} = route.params;
 
+  const images = [
+    product.image,
+    'https://images.pexels.com/photos/45982/pexels-photo-45982.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=0 ',
+    'https://images.pexels.com/photos/4066292/pexels-photo-4066292.jpeg?auto=compress&cs=tinysrgb&w=400',
+  ];
+
+
   return (
     <ScrollView style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={{uri: product.image}} style={styles.productImage} />
+          <ScrollView horizontal pagingEnabled>
+          {images.map((image: any, index: number) => (
+            <Image key={index} source={{ uri: image }} style={styles.productImage} />
+          ))}
+          </ScrollView>
       </View>
       <View style={styles.productInfo}>
         <Text style={styles.productTitle}>{product.title}</Text>
@@ -38,7 +49,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   productImage: {
-    width: windowWidth * 0.7,
+    width: windowWidth * 1,
     height: windowWidth * 0.7,
     resizeMode: 'contain',
   },
